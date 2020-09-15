a265d5e92a12ee17aa64e2c232d8b7b50fa9e296
whossname <tyson.buzza@gmail.com>
2020-09-15 10:22:09 +0800

    remove logs

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,6 @@
 
   let params = new URLSearchParams(document.location.search);
   let orderName = params.get("orderName");
-  console.dir(orderName);
   let buildOrder = null;
 
   const setBuildOrder = order => {
@@ -25,7 +24,6 @@
 
   if (orderName) {
     buildOrder = setBuildOrder(orderName);
-    console.dir(buildOrder);
   }
 </script>
 
